2d67d0235a4bf2db96be79029777dcf1613cbd56
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-01-14 15:15:39 -0700

    Words match regardless of diacritics

--- app/models/word.rb
+++ app/models/word.rb
@@ -1,3 +1,33 @@
+# frozen_string_literal: true
+
 class Word < ApplicationRecord
+  ALPHABET = ('a'..'z')
+  DIACRITICS_MAP = {
+    'á': 'a',
+    'â': 'a',
+    'ã': 'a',
+    'à': 'a',
+    'ç': 'c',
+    'é': 'e',
+    'ê': 'e',
+    'í': 'i',
+    'ó': 'o',
+    'ô': 'o',
+    'õ': 'o',
+    'ú': 'u'
+  }.freeze
+
   belongs_to :line
+
+  def match(guess)
+    clean(value) == clean(guess)
+  end
+
+  private
+
+  def clean(word)
+    word.downcase.chars.map do |character|
+      DIACRITICS_MAP[character.intern] || character
+    end.select { |character| ALPHABET.include?(character) }.join
+  end
 end
--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,46 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#match' do
+    let(:word) { Fabricate(:word, value: 'vós,') }
+    subject { word.match(guess) }
+
+    context 'with a bad guess' do
+      let(:guess) { 'nope' }
+
+      it { expect(subject).to be(false) }
+    end
+
+    context 'with a good, but incorrect guess' do
+      let(:guess) { 'nos' }
+
+      it { expect(subject).to be(false) }
+    end
+
+    context 'with an exact match' do
+      let(:guess) { 'vós,' }
+
+      it { expect(subject).to be(true) }
+    end
+
+    context 'with divergent capitalization' do
+      let(:guess) { 'Vós,' }
+
+      it { expect(subject).to be(true) }
+    end
+
+    context 'with differing punctuation' do
+      let(:guess) { 'vós' }
+
+      it { expect(subject).to be(true) }
+    end
+
+    context 'using only English letters' do
+      let(:guess) { 'vos,' }
+
+      it { expect(subject).to be(true) }
+    end
+  end
 end
