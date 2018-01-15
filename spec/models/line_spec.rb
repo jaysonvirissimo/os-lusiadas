fcfc46811cd374d0d529d146a41e279ffb44f25a
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-01-14 19:30:57 -0700

    Start line tests

--- spec/models/line_spec.rb
+++ spec/models/line_spec.rb
@@ -1,5 +1,13 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Line, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'with an attribute-less instance' do
+    it { expect(described_class.new).to_not be_valid }
+  end
+
+  describe '#words' do
+    it { expect(described_class.new.words).to be_empty }
+  end
 end
