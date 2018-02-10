17a17f05eac0125583b51ab8c49a9b6e726024ae
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-02-09 21:04:33 -0700

    Add helper to generate word elements

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,2 +1,9 @@
 module LinesHelper
+  def element_for(word)
+    content_tag(
+      :span,
+      content_tag(:span, word.first.value).concat(content_tag(:span, word.rest.value)),
+      class: 'word'
+    )
+  end
 end
