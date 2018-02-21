c13ffdc815b9b118e4705b7842229b8c474752b3
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-02-20 21:31:53 -0700

    Word fragment visibility has style consequences

--- app/helpers/lines_helper.rb
+++ app/helpers/lines_helper.rb
@@ -1,9 +1,19 @@
+# frozen_string_literal: true
+
 module LinesHelper
   def element_for(word)
     content_tag(
       :span,
-      content_tag(:span, word.first.value).concat(content_tag(:span, word.rest.value)),
+      content_tag(:span, word.first.value, style: style(word.first.visible)).concat(content_tag(:span, word.rest.value, style: style(word.rest.visible))),
       class: 'word'
     )
+  end
+
+  def style(visible)
+    if visible
+      'visibility: visible;'
+    else
+      'visibility: hidden;'
+    end
   end
 end
