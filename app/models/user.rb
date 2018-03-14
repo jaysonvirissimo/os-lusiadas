744a934ed63c47b8198475e5b9671c72f07f27fc
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-03-13 19:38:14 -0700

    Users know they first word to learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,12 +7,12 @@ class User < ApplicationRecord
   validates :name, presence: true
 
   def next_line_to_learn
-    if words.present?
-      Word.find_by(absolute_position: words.pluck(:absolute_position).max + 1).line
-    end
+    return Word.first_word unless words.count.positive?
+    Word.find_by(absolute_position: words.pluck(:absolute_position).max + 1).line
   end
 
   def next_line_to_review
+    return nil unless words.count.positive?
     words.review.min_by(&:absolute_position).line
   end
 
