8d242cfa028d422362155b57b1d496cf6b3a4330
Jayson Virissimo <jayson.virissimo@asu.edu>
2018-03-13 22:34:10 -0700

    Fix users controller specs

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -4,14 +4,22 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
   describe '#destroy' do
-    let(:id) { user.id }
-    let(:user) { Fabricate(:user) }
-    subject { delete :destroy, params: { id: id } }
+    context 'when signed in' do
+      let(:id) { user.id }
+      let(:user) { Fabricate(:user) }
+      subject { delete :destroy, params: { id: id } }
 
-    it 'deletes the given user' do
-      expect(User.find_by(id: id)).to be
-      expect(subject).to redirect_to(root_url)
-      expect(User.find_by(id: id)).to_not be
+      before do
+        allow_any_instance_of(described_class)
+          .to receive(:current_user)
+          .and_return(user)
+      end
+
+      it 'deletes the given user' do
+        expect(User.find_by(id: id)).to be
+        expect(subject).to redirect_to(root_url)
+        expect(User.find_by(id: id)).to_not be
+      end
     end
   end
 end
